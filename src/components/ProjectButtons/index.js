b9cbee8dd8eda9c85dc4890d5ecb18e1278ab032
Drew A <alvarez.drew@gmail.com>
2020-12-12 11:42:26 -0600

    resposive design css

--- src/components/ProjectButtons/index.js
+++ src/components/ProjectButtons/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ProjectButtons(props) {
     return (
-        <div style={{float: "left", margin: "0 0 2rem 0"}}>
+        <div style={{float: "left", margin: "0 1rem 2rem 0"}}>
             <button type="button" className="btn btn-outline-primary" onClick={props.handleOnClick}>{props.btnName}</button>
         </div>
     )
